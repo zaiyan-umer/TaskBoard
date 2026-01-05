fbdb9d3f693eca3e8689cf73e04136f87488b21d
zaiyan-umer <zaiyanumer6@gmail.com>
2026-01-05 17:39:45 +0500

    structure the app and design schemas

---
+++ app/models/task.ts
@@ -0,0 +1,46 @@
+import mongoose, { model, models, Schema } from "mongoose";
+
+interface TaskInterface{
+    title: string,
+    description: string,
+    status: 'todo' | 'in-progress' | 'done',
+    priority: 'high' | 'low' | 'medium',
+    dueDate: string,
+    owner: mongoose.Types.ObjectId,
+    createdAt? : Date,
+    updatedAt? : Date,
+    _id? : mongoose.Types.ObjectId
+}
+
+const TaskSchema = new Schema<TaskInterface>({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    status: {
+        type: String,
+        required: true,
+        default: 'todo'
+    },
+    priority: {
+        type: String,
+        required: true,
+        default: 'low'
+    },
+    dueDate: {
+        type: String,
+        default: 'no due date'
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+}, {timestamps: true})
+
+const Task = models?.task || model("task", TaskSchema);
+
+export default Task;
---
+++ app/models/user.ts
@@ -0,0 +1,52 @@
+import mongoose, { model, models, Schema } from "mongoose";
+import bcrypt from 'bcrypt'
+
+interface IUser {
+    name: string,
+    email: string,
+    password: string,
+    role: "admin" | "user",
+    createdAt?: Date,
+    updatedAt?: Date,
+    _id?: mongoose.Types.ObjectId
+}
+
+const UserSchema = new Schema<IUser>({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: {
+            validator: function(value: string) {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+            },
+            message: 'Invalid email format'
+        }
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true,
+        default: "user"
+    }
+}, {timestamps: true})
+
+UserSchema.pre('save', async function(){
+    if(this.isModified('password')){
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+})
+
+const User = models?.user || model("user", UserSchema);
+
+export default User
